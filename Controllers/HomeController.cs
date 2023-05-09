eb4417301227299a937772337516722be274c669
AGOSTINA BOTTARINI <47866440@ort>
2023-05-09 10:14:44 -0300

    Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using TP04_Bottarini_Piva.Models;
 namespace TP04_Bottarini_Piva.Controllers;
 
 public class HomeController : Controller
 {
     public IActionResult Index()
     {
+        ViewBag.DicIndumentaria = Equipos.EquiposIndumentaria;
         return View();
+    }
+
+    public IActionResult SelectIndumentaria()
+    {
+        ViewBag.ListaEquipos = Equipos.ListaEquipos;
+        ViewBag.ListaMedias = Equipos.ListaMedias;
+        ViewBag.ListaPantalones = Equipos.ListaPantalones;
+        ViewBag.ListaCamisetas = Equipos.ListaCamisetas;
+        return View();
+    }
+
+    public IActionResult GuardarIndumentaria (int Equipo, int Media, int Pantalon, int Camiseta)
+    {
+        Indumentaria nuevaIndumentaria = new Indumentaria(Equipos.ListaMedias[Media], Equipos.ListaPantalones[Pantalon], Equipos.ListaCamisetas[Camiseta]);
+        
+        Equipos.EquiposIndumentaria.Add(Equipos.ListaEquipos[Equipo], nuevaIndumentaria);
+
+        return View("Index");
     }
 }
--- Models/Equipos.cs
+++ Models/Equipos.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Equipos.Models
+namespace TP04_Bottarini_Piva.Models
 {
     public static class Equipos
 {
     public static List<string> ListaEquipos {get; private set;} = new List<string>(){"River", "Boca", "Huracan", "Ferro", "San Lorenzo", "Racing", "Estudiantes de La Plata", "Independiente", "Belgrano", "Talleres"};
     public static List<string> ListaMedias {get; private set;} = new List<string>(){"medias_amarillas.jpg", "medias_azules.jpg", "medias_blancas.jpg", "medias_celestes.jpg", "medias_grises.jpg", "medias_naranjas.jpg", "medias_negras.jpg", "medias_rojas.jpg", "medias_verdes.jpg", "medias_violetas.jpg"};
     public static List<string> ListaPantalones {get; private set;} = new List<string>(){"pantalon_amarillo.jpg", "pantalon_azul.jpg", "pantalon_blanco.jpg", "pantalon_celeste.jpg", "pantalon_grises.jpg", "pantalon_naranja.jpg", "pantalon_negro.jpg", "pantalon_rojo.jpg", "pantalon_verde.jpg", "pantalon_violeta.jpg"};
-    public static List<string> ListaRemeras {get; private set;} = new List<string>(){"camiseta_amarilla.jpg", "camiseta_azul.jpg", "camiseta_blanca.jpg", "camiseta_celeste.jpg", "camiseta_gris.jpg", "camiseta_naranja.jpg", "camiseta_negra.jpg", "camiseta_roja.jpg", "camiseta_verde.jpg", "camiseta_violeta.jpg"};
+    public static List<string> ListaCamisetas {get; private set;} = new List<string>(){"camiseta_amarilla.jpg", "camiseta_azul.jpg", "camiseta_blanca.jpg", "camiseta_celeste.jpg", "camiseta_gris.jpg", "camiseta_naranja.jpg", "camiseta_negra.jpg", "camiseta_roja.jpg", "camiseta_verde.jpg", "camiseta_violeta.jpg"};
     public static Dictionary<string, Indumentaria> EquiposIndumentaria {get; private set;} = new Dictionary<string, Indumentaria>();
 
     public static bool IngresarIndumentaria(string EquipoSeleccionado, Indumentaria item)
